706afac2c11eb06bb6937620c7afcb7b970895d9
Gratitech <123098943+GratitechInc@users.noreply.github.com>
2023-05-17 03:20:10 -0500

    Update s3cr3txNodeJS_OpenAI_API_Key_ProtectionDemo.js

--- s3cr3txNodeJS_OpenAI_API_Key_ProtectionDemo.js
+++ s3cr3txNodeJS_OpenAI_API_Key_ProtectionDemo.js
@@ -1,90 +1,62 @@
-var https = require('https');
-
-var openai_host = 'api.openai.com';
-var s3cr3tx_host = 's3cr3tx.com';
+const fetch = require("node-fetch");
+const openai_host = 'https://api.openai.com/v1/models';
 var s3cr3tx_email = process.env.s3cr3tx_email;
 var s3cr3tx_token = process.env.s3cr3tx_token;
 var s3cr3tx_code = process.env.s3cr3tx_code;
-openai_apiKey = process.env.openai_apiKey;
-
-var options = {
-    host : String(openai_host), // here only the domain name
-    // (no http/https !)
-    port : 443,
-    path : '/v1/models', // the rest of the url with parameters if needed
-    method : 'GET', // do GET
-    headers : {'Authorization':'Bearer '+ String(openai_apiKey)}
-};
-
-console.info('Options prepared for first OpenAI API call:');
-console.info(options);
-console.info('Do the First GET call to OpenAI API');
-
-// do the GET request With clear text API Key stored in env variable
-var reqGet = https.request(options, function(res) {
-    console.log("statusCode: ", res.statusCode);
-    res.on('data', function(d) {
-        console.info('First GET result:\n');
-        process.stdout.write(d);
-        console.info('\n\nCall completed');
-    });
-
-});
-
-reqGet.end();
-reqGet.on('error', function(e) {
-    console.error(e);
-});
-function getS3cr3tx(input,EoD){
-    var s3cr3txOptions = {
-        host : String(s3cr3tx_host), // here only the domain name
-        port : 443,
-        path : '/Values', // the rest of the url with parameters if needed
-        method : 'GET', // do GET
-        headers : {'Email':String(s3cr3tx_email),
-        'APIToken':String(s3cr3tx_token),
-        'AuthCode':String(s3cr3tx_code),
-        'Input':String(input),
-        'EoD':String(EoD)
-        }
-    };
-    var s3cr3txreqGet = https.request(s3cr3txOptions, function(res) {
-        console.log("statusCode Fromm S3cr3tx.com : ", res.statusCode);
-        res.on('data', function(d) {
-            return String(d);
+var openai_apiKey = process.env.openai_apiKey;
+async function fetchS3cr3tx(input,direction) {
+        try {
+            const response = await fetch(`https://s3cr3tx.com/Values`, {
+                method: 'GET',
+                headers:{'Email':String(s3cr3tx_email),
+                'APIToken':String(s3cr3tx_token),
+                'AuthCode':String(s3cr3tx_code),
+                'Input':String(input),
+                'EorD':String(direction)
+                }
             });
-       });
-    s3cr3txreqGet.end();
-    s3cr3txreqGet.on('error', function(e) {
-    console.error(e);
-});
+            const result = await response.text();
+            return result;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+async function getS3cr3tx(input,direction) {
+        var s3cr3tx = await fetchS3cr3tx(input,direction);
+        return s3cr3tx;
+    }
+    async function fetchOpenAImodels() {
+        try {
+            const response = await fetch(openai_host, {
+                method: 'GET',
+                headers:{'Authorization':'Bearer ' + process.env.openai_apiKey
+                }
+            });
+            const result = await response.text();
+            return result;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+async function fetchOpenAImodelsProtected() {
+        try {
+            const response = await fetch(openai_host, {
+                method: 'GET',
+                headers:{'Authorization':'Bearer ' + await getS3cr3tx(process.env.s3cr3tx_openAIkey,'d')
+                }
+            });
+            const result = await response.text();
+            return result;
+        } catch (error) {
+            console.error(error);
+        }
+    }
+async function testS3cr3tx(){
+    var s3cr3tx_openAIkey = await getS3cr3tx(process.env.openai_apiKey,'e');
+    process.env.s3cr3tx_openAIkey = s3cr3tx_openAIkey;
+var OpenAIresponse1= await fetchOpenAImodels();
+var OpenAIresponse2 = await fetchOpenAImodelsProtected();
+    console.log("First Response: " + OpenAIresponse1)
+    console.log("2nd Response: " + OpenAIresponse2)
 }
-process.env.s3cr3tx_Protected_OpenAI_Key = getS3cr3tx(process.env.openai_apiKey,'e');
-var options2 = {
-    host : String(openai_host), // here only the domain name
-    // (no http/https !)
-    port : 443,
-    path : '/v1/models', // the rest of the url with parameters if needed
-    method : 'GET', // do GET
-    headers : {'Authorization':'Bearer '+ getS3cr3tx(String(process.env.s3cr3tx_Protected_OpenAI_Key),'d')}
-};
-
-console.info('Options prepared For 2nd call to OpenAI API with s3cr3tx Protection:');
-console.info(options);
-console.info('Do the 2nd GET call using protectd API Key Env Var');
-
-// do the GET request With S3cr3tx Protected API Key stored in env variable
-var reqGet = https.request(options, function(res) {
-    console.log("statusCode: ", res.statusCode);
-    res.on('data', function(d) {
-        console.info('GET 2nd call to OpenAI results:\n');
-        process.stdout.write(d);
-        console.info('\n\n2nd Call to OpemAI with Protected API Key completed');
-    });
-
-});
-
-reqGet.end();
-reqGet.on('error', function(e) {
-    console.error(e);
-});
+testS3cr3tx();
